895bfc5e4f86188ade3f3138d0b0715fcfce5bc5
JoseD <95452898+JoseD2021@users.noreply.github.com>
2024-10-17 15:57:40 -0500

    Funcion distancia, creacion del grafo

--- Graph.py
+++ Graph.py
@@ -1,29 +1,32 @@
 from typing import List
 import heapq
+import math
+
 class Graph:
-  def _init_ (self, n: int, directed: bool = False):
-    self.n = n
-    self.directed = directed
-    self.L: List[List[int]] = [[] for _ in range(n)]
+    R = 6378 #6371 # Radio promedio de la tierra, 6378 radio ecuatorial
+    def __init__ (self, n: int, directed: bool = False):
+        self.n = n
+        self.directed = directed
+        self.L: List[List[int]] = [[] for _ in range(n)]
 
-  def add_edge(self, u: int, v: int, weight: float) -> bool:
-    if 0 <= u < self.n and 0 <= v < self.n:
-        self.L[u].append((v, weight))  # Guardar el peso
-        if not self.directed:
-            self.L[v].append((u, weight))
-        return True
-    return False
+    def add_edge(self, u: int, v: int, weight: float = 1) -> bool:
+        if 0 <= u < self.n and 0 <= v < self.n:
+            self.L[u].append((v, weight)) # Guardar el peso
+            if not self.directed:
+                self.L[v].append((u, weight))
+            return True
+        return False
 
 
-  def DFS(self, u: int) -> None:
-    visit = [False for _ in range(self.n)]
-    self.__DFS_visit(u, visit)
-    print()
+    def DFS(self, u: int) -> None:
+        visit = [False for _ in range(self.n)]
+        self.__DFS_visit(u, visit)
+        print()
 
-  # Calcular las componentes conexas y el número de vértices en cada una
-  def connected_components(self):
-        visit = [False] * self.n  # Lista para marcar los nodos visitados
-        components = []  # Lista de componentes, cada componente es una lista de vértices
+    # Calcular las componentes conexas y el número de vértices en cada una
+    def connected_components(self):
+        visit = [False] * self.n # Lista para marcar los nodos visitados
+        components = [] # Lista de componentes, cada componente es una lista de vértices
         
         for u in range(self.n):
             if not visit[u]:
@@ -41,145 +44,181 @@ class Graph:
                 print(f"Componente {i + 1} tiene {len(component)} vértices: {component}")
     
     # DFS modificado para recolectar todos los vértices de una componente
-  def __DFS_collect(self, u: int, visit: List[bool], component: List[int]) -> None:
+    def __DFS_collect(self, u: int, visit: List[bool], component: List[int]) -> None:
         visit[u] = True
         component.append(u)
-        for v, _ in self.L[u]:  # Recorrer solo los vecinos
+        for v, _ in self.L[u]: # Recorrer solo los vecinos
             if not visit[v]:
-                self.__DFS_collect(v, visit, component)  
+                 self.__DFS_collect(v, visit, component)    
 
-  def __DFS_visit(self, u: int, visit: List[bool], Print: bool = True) -> List:
-    visit[u] = True
-    if Print:
-        print(u, end=' ')
-    for v, _ in self.L[u]:  # Ajustar para tomar solo el vértice v
-        if not visit[v]:
-            visit = self.__DFS_visit(v, visit, Print)
-    return visit
-
-
-  def BFS(self, u: int):
-    queue = []
-    visit = [False] * self.n
-    visit[u] = True
-    queue.append(u)
-    while queue:
-        u = queue.pop(0)
-        print(u, end=' ')
-        for v, _ in self.L[u]:  # Ajustar para tomar solo el vértice v
+    def __DFS_visit(self, u: int, visit: List[bool], Print: bool = True) -> List:
+        visit[u] = True
+        if Print:
+            print(u, end=' ')
+        for v, _ in self.L[u]:    # Ajustar para tomar solo el vértice v
             if not visit[v]:
-                visit[v] = True
-                queue.append(v)
+                visit = self.__DFS_visit(v, visit, Print)
+        return visit
 
 
-  def degree(self, u: int) -> int:
-    return len(self.L[u])
+    def BFS(self, u: int):
+        queue = []
+        visit = [False] * self.n
+        visit[u] = True
+        queue.append(u)
+        while queue:
+            u = queue.pop(0)
+            print(u, end=' ')
+            for v, _ in self.L[u]:    # Ajustar para tomar solo el vértice v
+                if not visit[v]:
+                    visit[v] = True
+                    queue.append(v)
 
-  def min_degree(self) -> int:
-    min = self.n
-    for u in range(self.n):
-      if self.degree(u) < min:
-        min = self.degree(u)
-    return min
 
-  def max_degree(self) -> int:
-    max = 0
-    for u in range(self.n):
-      if self.degree(u) > max:
-        max = self.degree(u)
-    return max
+    def degree(self, u: int) -> int:
+        return len(self.L[u])
 
-  def degree_sequence(self) -> List[int]:
-    seq = []
-    for u in range(self.n):
-      seq.append(self.degree(u))
-    seq.sort(reverse = True)
-    return seq
+    def min_degree(self) -> int:
+        min = self.n
+        for u in range(self.n):
+            if self.degree(u) < min:
+                min = self.degree(u)
+        return min
 
-  # calcular k(g) numero de componentes conexos de g
-  def number_of_components(self) -> int:
-    visit = [False] * self.n
-    count = 0
-    for u in range(self.n):
-      if not visit[u]:
-        visit = self.__DFS_visit(u, visit, False)
-        count += 1
-    return count
+    def max_degree(self) -> int:
+        max = 0
+        for u in range(self.n):
+            if self.degree(u) > max:
+                max = self.degree(u)
+        return max
 
-  # g es conexo?
+    def degree_sequence(self) -> List[int]:
+        seq = []
+        for u in range(self.n):
+            seq.append(self.degree(u))
+        seq.sort(reverse = True)
+        return seq
 
-  def is_connected(self) -> bool:
-    return self.number_of_components() == 1
+    # calcular k(g) numero de componentes conexos de g
+    def number_of_components(self) -> int:
+        visit = [False] * self.n
+        count = 0
+        for u in range(self.n):
+            if not visit[u]:
+                visit = self.__DFS_visit(u, visit, False)
+                count += 1
+        return count
 
-  def is_eulerian(self) -> bool:
-    for u in range(self.n):
-      if self.degree(u) % 2 == 1:
-        return False
-    return True
+    # g es conexo?
 
-  def is_semieulerian(self) -> bool:
-    count = 0
-    for u in range(self.n):
-      if self.degree(u) % 2 == 1:
-        count += 1
-      if count > 2:
-        return False
+    def is_connected(self) -> bool:
+        return self.number_of_components() == 1
 
-  def is_r_regular(self, r: int) -> bool:
-    for u in range(self.n):
-      if self.degree(u) != r:
-        return False
-    return True
-
-  def is_complete(self) -> bool:
-    for u in range(self.n):
-        for v in range(self.n):
-            if u != v and v not in [vertex for vertex, _ in self.L[u]]:
+    def is_eulerian(self) -> bool:
+        for u in range(self.n):
+            if self.degree(u) % 2 == 1:
                 return False
-    return True
+        return True
+
+    def is_semieulerian(self) -> bool:
+        count = 0
+        for u in range(self.n):
+            if self.degree(u) % 2 == 1:
+                count += 1
+            if count > 2:
+                return False
+
+    def is_r_regular(self, r: int) -> bool:
+        for u in range(self.n):
+            if self.degree(u) != r:
+                return False
+        return True
+
+    def is_complete(self) -> bool:
+        for u in range(self.n):
+            for v in range(self.n):
+                if u != v and v not in [vertex for vertex, _ in self.L[u]]:
+                    return False
+        return True
 
 
-  def is_acyclic(self) -> bool:
-    for u in range(self.n):
-      for v in self.L[u]:
-        if self.path(u, v):
-          return False
-    return True
-  
-  def prim_mst(self) -> float:
-    # Si el grafo no es conexo, no se puede calcular el MST completo
-    if not self.is_connected():
-        return "El grafo no es conexo. No se puede calcular el MST completo."
+    def is_acyclic(self) -> bool:
+        for u in range(self.n):
+            for v in self.L[u]:
+                if self.path(u, v):
+                    return False
+        return True
     
-    # El peso total del MST
-    mst_weight = 0
-    visited = [False] * self.n
-    min_heap = [(0, 0)]  # (peso, nodo de inicio)
-    heapq.heapify(min_heap)
+    def prim_mst(self) -> float:
+        # Si el grafo no es conexo, no se puede calcular el MST completo
+        #if not self.is_connected():
+        #    return "El grafo no es conexo. No se puede calcular el MST completo."
+        
+        # El peso total del MST
+        mst_weight = 0
+        visited = [False] * self.n
+        min_heap = [(0, 0)]    # (peso, nodo de inicio)
+        heapq.heapify(min_heap)
 
-    # Contador para asegurarnos de que conectamos todos los nodos
-    edges_used = 0
+        # Contador para asegurarnos de que conectamos todos los nodos
+        edges_used = 0
 
-    while min_heap and edges_used < self.n:
-        weight, u = heapq.heappop(min_heap)
-        if visited[u]:
-            continue
+        while min_heap and edges_used < self.n:
+            weight, u = heapq.heappop(min_heap)
+            if visited[u]:
+                    continue
+            # Marcar el nodo como visitado
+            visited[u] = True
+            mst_weight += weight
+            edges_used += 1
 
-        # Marcar el nodo como visitado
-        visited[u] = True
-        mst_weight += weight
-        edges_used += 1
+            # Explorar las aristas conectadas al nodo u
+            for v, edge_weight in self.L[u]: # Descomponer la tupla correctamente
+                if not visited[v]:
+                    heapq.heappush(min_heap, (edge_weight, v))
 
-        # Explorar las aristas conectadas al nodo u
-        for v, edge_weight in self.L[u]:  # Descomponer la tupla correctamente
-            if not visited[v]:
-                heapq.heappush(min_heap, (edge_weight, v))
+        # Retornar el peso total del MST
+        return mst_weight
 
-    # Retornar el peso total del MST
-    return mst_weight
+""" # Ejemplo dijkstra
 
-  
-  # Crear un grafo con 5 nodos
+    def dijkstra(self, start_vertex_data):
+        start_vertex = self.vertex_data.index(start_vertex_data)
+        distances = [float('inf')] * self.n
+        distances[start_vertex] = 0
+        visited = [False] * self.n
+
+        for _ in range(self.n):
+            min_distance = float('inf')
+            u = None
+            for i in range(self.n):
+                if not visited[i] and distances[i] < min_distance:
+                    min_distance = distances[i]
+                    u = i
+
+            if u is None:
+                break
+
+            visited[u] = True
+
+            for v in range(self.n):
+                if self.adj_matrix[u][v] != 0 and not visited[v]:
+                    alt = distances[u] + self.adj_matrix[u][v]
+                    if alt < distances[v]:
+                        distances[v] = alt
+
+        return distances
+    
+    def calcDistance(self,lat1,long1,lat2,long2): # Fórmula del Haversine
+        lat = lat2 - lat1
+        long = long2 - long1
+        a = math.sin(lat/2)**2 + math.cos(lat1)*math.cos(lat2)*(math.sin(long/2)**2)
+        c = 2*math.atan2(math.sqrt(a),math.sqrt(1-a))
+        d = self.R*c
+        return d
+"""
+"""
+    # Crear un grafo con 5 nodos
 g = Graph(5)
 
 # Agregar aristas con pesos (u, v, peso)
@@ -190,7 +229,7 @@ g.add_edge(1, 3, 8.0)
 g.add_edge(1, 4, 5.0)
 g.add_edge(2, 4, 7.0)
 g.add_edge(3, 4, 9.0)
-
+# 
 # Calcular el peso del árbol de expansión mínima
 mst_weight = g.prim_mst()
 print(f"El peso del Árbol de Expansión Mínima es: {mst_weight}")
@@ -209,3 +248,5 @@ g.add_edge(3, 4, 5.0)
 
 # Determinar si el grafo es conexo y, si no lo es, el número de componentes
 g.connected_components()
+
+"""
--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+from Graph import Graph
+import pandas as pd
 
+
+df = pd.read_csv("flights_final.csv")
+
+
+codes = {} # Registro clave = Int vertice, Valor = Airport Code
+codesI = 0
+
+for _,e in df.iterrows(): # recorre cada fila del csv
+    if not e["Source Airport Code"] in codes:
+        codes[e["Source Airport Code"]] = codesI
+        codesI+=1
+    if not e["Destination Airport Code"] in codes:
+        codes[e["Destination Airport Code"]] = codesI
+        codesI+=1
+
+g = Graph(len(codes))
+for _,e in df.iterrows(): # recorre cada fila del csv
+    # Agrega todas las aristas
+    g.add_edge(codes[e["Source Airport Code"]], codes[e["Destination Airport Code"]], g.calcDistance(e["Source Airport Longitude"],e["Source Airport Latitude"],e["Destination Airport Longitude"],e["Destination Airport Latitude"]))
+
+# print(g.number_of_components())
+g.connected_components()
+# print(codes)
